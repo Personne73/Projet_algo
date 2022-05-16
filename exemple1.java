cd8e00177bd64f23b557dc8c069865f847b4e64c
Joël Mbiapa <94489918+Personne73@users.noreply.github.com>
2022-05-16 22:20:52 +0200

    pas de modif

--- exemple1.java
+++ exemple1.java
@@ -1,7 +1,7 @@
 public class exemple1{
 
     static final int plusInfini = Integer.MAX_VALUE, moinsInfini = Integer.MIN_VALUE;
-    
+
     public static void main(String[] args){
         System.out.println("Hello World");
     }
