a51a5678154568a9e8e66682f51bacb1090956db
lambertb <benjamin.lambert@edu.esiee.fr>
2022-05-22 00:12:25 +0200

    ajout + modif code

--- SacDeValeurMaximum.java
+++ SacDeValeurMaximum.java
@@ -29,41 +29,27 @@ public class SacDeValeurMaximum {
 		System.out.println();
     }
 
-    
-    public static void afficher(int[][] M){ int n = M.length; // affichage du tableau M.
-        System.out.println("\t[");
-        for (int i = n-1; i>=0; i--) 
-            System.out.println("\t\t" + Arrays.toString(M[i]));
-       System.out.println("\t]");
-    }
+    public static int[][] calculerM(int[] V, int[] T, int C){
+        int n = V.length;
+        // Retourne M[0:n+1][0:C+1], de terme général M[k][c] = m(k,c)
+        int[][] M = new int[n+1][C+1];
 
-    public static int somme(int[] T){
-       int s = 0; 
-       for (int i = 0; i<T.length; i++) 
-           s = s+T[i]; 
-       return s;
-    }
-   
-    public static int[][] calculerM(int[] V, int[] T, int C){int n = V.length;
-    // Retourne M[0:n+1][0:C+1], de terme général M[k][c] = m(k,c)
-    int[][] M = new int[n+1][C+1];
-    // Base : m(0,c) = 0 pour toute contenance c, 0 ≤ c < C+1
-    for (int c = 0; c < C+1; c++) M[0][c] = 0;
-    // Cas général, pour tous k et c, 1 ≤ k < n+1, 0 ≤ c < C+1,
-    // m(k,c) = max(M[k-1][c], V[k-1] + M[k-1][c-T[k-1]])
-    for (int k = 1; k < n+1; k++)
-        for (int c = 0; c < C+1; c++) // calcul et mémorisation de m(k,c)
-            if (c-T[k-1] < 0) // le k-ème objet est trop gros pour entrer dans le sac 
-                M[k][c] = M[k-1][c];
-            else  
-                M[k][c] = max(M[k-1][c], V[k-1]+M[k-1][c-T[k-1]]);
+        // Base : m(0,c) = 0 pour toute contenance c, 0 ≤ c < C+1
+        for (int c = 0; c < C+1; c++) M[0][c] = 0;
+
+        // Cas général, pour tous k et c, 1 ≤ k < n+1, 0 ≤ c < C+1,
+        // m(k,c) = max(M[k-1][c], V[k-1] + M[k-1][c-T[k-1]])
+        for (int k = 1; k < n+1; k++)
+            for (int c = 0; c < C+1; c++) // calcul et mémorisation de m(k,c)
+                if (c-T[k-1] < 0) // le k-ème objet est trop gros pour entrer dans le sac 
+                    M[k][c] = M[k-1][c];
+                else  
+                    M[k][c] = max(M[k-1][c], V[k-1]+M[k-1][c-T[k-1]]);
         return M;
     }
    
-   
-    public static int max(int x, int y){ 
-        if (x >= y) return x; 
-        return y; 
+    public static int calculerMGlouton(int[] V, int[] T, int C){
+        return 0;
     }
 
     public static void asm(int[][] M, int[] V, int[] T, int k, int c){
@@ -81,6 +67,25 @@ public class SacDeValeurMaximum {
                k-1, V[k-1], T[k-1]); // Le k-ème objet été affiché
             // svm(k-1,c-t(k-1)) union {k-1} a été affiché, donc svm(k,c) a été affiché.
         }
-   }
+    }
+
+    public static void afficher(int[][] M){ int n = M.length; // affichage du tableau M.
+        System.out.println("\t[");
+        for (int i = n-1; i>=0; i--) 
+            System.out.println("\t\t" + Arrays.toString(M[i]));
+        System.out.println("\t]");
+    }
+
+    public static int somme(int[] T){
+        int s = 0; 
+        for (int i = 0; i<T.length; i++) 
+            s = s+T[i]; 
+        return s;
+    }
+
+    public static int max(int x, int y){ 
+        if (x >= y) return x; 
+        return y; 
+    }
 
 }
